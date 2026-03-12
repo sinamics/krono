0f686631faeb88262e5b46bc328baf2f3666ecf5
Bernt Christian Egeland <bernt.christian.egeland@gmail.com>
2026-03-12 15:15:42 +0000

    table

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -29,7 +29,7 @@ export default async function ProtectedLayout({
       }
     >
       <AppSidebar user={session.user} />
-      <SidebarInset>
+      <SidebarInset className="overflow-hidden">
         <header className="flex h-16 shrink-0 items-center gap-2 px-4">
           <SidebarTrigger className="-ml-1" />
           <Separator
@@ -38,7 +38,7 @@ export default async function ProtectedLayout({
           />
           <SidebarBreadcrumb />
         </header>
-        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+        <div className="flex min-h-0 flex-1 flex-col gap-4 overflow-hidden p-4 pt-0">
           {children}
         </div>
       </SidebarInset>
