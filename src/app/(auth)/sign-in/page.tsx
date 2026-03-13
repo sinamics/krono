c01064874b6a5b6f0e1292a13a8ea1b0170ba355
Bernt Christian Egeland <bernt.christian.egeland@gmail.com>
2026-03-13 11:49:45 +0000

    connection

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,7 +1,9 @@
+import { connection } from "next/server";
 import { db } from "@/lib/db";
 import { LoginForm } from "@/features/auth/Components/LoginForm";
 
 export default async function SignInPage() {
+  await connection();
   const regSetting = await db.appSettings.findUnique({
     where: { key: "registration_enabled" },
   });
